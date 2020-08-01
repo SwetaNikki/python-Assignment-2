3ee05fb589ecc534ca6e1e166ed2eda81a45d931
SwetaNikki <56434154+SwetaNikki@users.noreply.github.com>
2020-08-01 16:40:18 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,1 +1,24 @@
+#Create the below pattern using nested for loop in Python.
+#     *
+#     **
+#     ***
+#     ****
+#     *****
+#     ****
+#     ***
+#     **
+#     *
 
+n=5;
+
+# '*' in increasing order from 0 to 5
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+# '*' in increasing order from 5 to 0
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
