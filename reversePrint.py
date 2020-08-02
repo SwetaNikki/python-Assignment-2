f26991eb789d0d63394ad1db21ebfda8e2f1bbb7
SwetaNikki <56434154+SwetaNikki@users.noreply.github.com>
2020-08-02 12:24:38 +0530

    Update reversePrint.py

--- reversePrint.py
+++ reversePrint.py
@@ -1,1 +1,13 @@
 
+# Write a Python program to reverse a word after accepting the input from the user.
+#Input word: ineuron
+#Output: norueni
+
+txt = input("Enter any string : ")
+print("Input : {}".format(txt))
+
+rev = ""
+for i in txt:
+    rev = i + rev
+    
+print("Output : {}".format(rev))
